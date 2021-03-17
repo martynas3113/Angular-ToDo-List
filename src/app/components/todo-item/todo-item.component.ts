a94fb0d744e34a9ce7fa4fc2d5c8f7433b55d72c
martynas3113 <martynas.pazusis@gmail.com>
2021-03-17 23:02:47 +0200

    put request back to API server to change value

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Todo } from 'src/app/models/Todo';
+import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-todo-item',
@@ -9,7 +10,7 @@ import { Todo } from 'src/app/models/Todo';
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
 
-  constructor() { }
+  constructor(private todoService:TodoService) { }
 
   ngOnInit(): void {
   }
@@ -25,7 +26,13 @@ export class TodoItemComponent implements OnInit {
 
   //onToggle
   onToggle(todo){
+    // Toggle in UI
     todo.completed = !todo.completed;
+    //Toggle on server
+    this.todoService.toggleCompleted(todo).subscribe(todo =>{
+      
+    })
+
   }
   //onDelete
   onDelete(todo){
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,28 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Todo } from '../models/Todo';
+import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  todosUrl:string = 'https://jsonplaceholder.typicode.com/todos';
+  todosLimit = '?_limit=5';
 
-  constructor() { }
 
-    getTodos(){
-      return [{
-        id:1,
-        title: 'Todo One',
-        completed: false
-      },
-      {
-        id:2,
-        title: 'Todo Two',
-        completed: false
-      },
-      {
-        id:3,
-        title: 'Todo Three',
-        completed: false
-      }
-    ]
+  constructor(private http:HttpClient) { }
+    //Get Todos
+    getTodos():Observable<Todo[]>{
+     return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
   }
+  //Toggle Completed
+  toggleCompleted(todo:Todo):Observable<any>{
+    const url = `${this.todosUrl}/${todo.id}`
+    return this.http.put(url, todo, httpOptions);
+   }
 }
